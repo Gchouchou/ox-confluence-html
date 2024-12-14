c1f5ba8e90790dabf048be9ae254c3cc027b7877
Justin <jusytinyu@gmail.com>
2024-12-13 21:08:28 -0500

    Initial Commit

---
+++ ox-confluence.el
@@ -0,0 +1,13 @@
+;;; ox-confluence --- Confluence Wiki Back-End for Org Export Engine
+
+;; Author: Justin Yu <jusytinyu@gmail.com>
+;; Keywords: confluence, wiki
+
+;; This file is not part of GNU Emacs.
+
+;;; Code:
+(require 'ox-html)
+
+
+(provide 'ox-confluence)
+;;; ox-confluence.el ends here
